eb4837680e8e13ff764414a1cce936cf99acd25a
Olivier Filangi <olivier.filangi@inrae.fr>
2026-06-02 16:03:11 +0200

    fix test with resource example.ttl

--- shared/src/main/scala/fr/inrae/metabohub/semantic_web/event/Subscriber.scala
+++
@@ -1,5 +0,0 @@
-package fr.inrae.metabohub.semantic_web.event
-
-trait Subscriber[-Event,-Publisher] {
-  def notify (pub: Publisher, event: Event): Unit
-}
--- shared/src/main/scala/fr/inrae/metabohub/semantic_web/exception/SWStatementConfigurationException.scala
+++
@@ -1,4 +0,0 @@
-package fr.inrae.metabohub.semantic_web.exception
-
-final case class SWStatementConfigurationException(private val message: String = "",
-                                                 private val cause: Throwable = None.orNull) extends Exception(message,cause)
